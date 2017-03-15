ea38acb4845b9e53432c99cf1a996ceff6d76db5
Matúš Cimerman <matus.cimerman@exponea.com>
2017-03-15 22:34:11 +0100

    Add basic thresholding

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,43 @@
 #include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 using namespace cv;
 using namespace std;
 
-int main() {
-    Mat image;
-    image = imread("../../data/5-6/005-Z_cut.tif", IMREAD_COLOR);
+Mat imgOriginal, imgThreshold;
+string originalWindow = "Original", thresholdWindow = "Threshold";
+string trackbarThresholdValue = "Threshold value", trackbarThresholdType = "Threshold type";
+int maxThresholdValue = 255, thresholdValue = 0, maxThresholdType = 4, thresholdType = 3;
 
-    if (image.empty()) {
-        cout << "Could not open image!" << std::endl;
+void thresholdCallback(int, void*);
+
+int main() {
+    imgOriginal = imread("../../data/5-6/005-Z_cut.tif", IMREAD_GRAYSCALE); // load image in grayscale
+
+    if (imgOriginal.empty()) {
+        cout << "Could not open imgOriginal!" << std::endl;
         return -1;
     }
 
-    std::cout << image.at<int>(0, 1);
+    // Window with original image.
+    namedWindow(originalWindow, WINDOW_AUTOSIZE);
+    imshow(originalWindow, imgOriginal);
 
-    namedWindow("Test image", WINDOW_AUTOSIZE);
-    imshow("Test image", image);
+    // Window with image after threshold.
+    namedWindow(thresholdWindow, WINDOW_AUTOSIZE);
+    createTrackbar(trackbarThresholdValue, originalWindow, &thresholdValue, maxThresholdValue, thresholdCallback);
+    createTrackbar(trackbarThresholdType, originalWindow, &thresholdType, maxThresholdType, thresholdCallback);
 
     waitKey(0);
     return 0;
 }
+
+void thresholdCallback(int, void*)
+{
+    cout << "threshold value: " << thresholdValue << endl;
+    threshold(imgOriginal, imgThreshold, thresholdValue, 255, thresholdType);
+    imshow(thresholdWindow, imgThreshold);
+}
+
